5f2eb1e3daf975b110e06d0d1dbdfe7f3ed2030c
Mashi0629 <poboshiniomashi@gmail.com>
2026-03-18 10:48:04 +0530

    fixing errors

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// Connect DB
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Quiz API Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
