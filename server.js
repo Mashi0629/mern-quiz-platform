d81357fb7f16cf7729d870402087e37fc3ea3157
Mashi0629 <poboshiniomashi@gmail.com>
2026-03-22 10:37:44 +0530

    Connect Routes in server.js

--- server.js
+++ server.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api/users", userRoutes);
