48c560f5e3726c4b6f6c22fa3787ad544dd7f769
Mashi0629 <poboshiniomashi@gmail.com>
2026-03-23 09:55:30 +0530

    Update REGISTER (hash password)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,31 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-// Signup
+
+// Signup (REGISTER)
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
-  const user = new User({ name, email, password });
-  await user.save();
+  try {
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  res.json({ message: "User registered" });
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    res.json({ message: "User registered" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Get all users
