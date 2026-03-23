580972f3a07f7c4f2e092f19d9b518d8d7568600
Mashi0629 <poboshiniomashi@gmail.com>
2026-03-23 10:27:43 +0530

    Move routes above app.listen()

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-
-dotenv.config();
 
 const app = express();
 
@@ -13,6 +10,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Quiz API Running 🚀");
@@ -23,7 +24,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const userRoutes = require("./routes/userRoutes");
-
-app.use("/api/users", userRoutes);
