d20ec4ebc035b836794736e9885d64bd5f4221ef
Mashi0629 <poboshiniomashi@gmail.com>
2026-03-23 10:16:38 +0530

    Add LOGIN API

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -28,6 +28,38 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// LOGIN
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    res.json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 // Get all users
 router.get("/", async (req, res) => {
   const users = await User.find();
