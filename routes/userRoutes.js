7ee6e4b33daad17cfa15e122fc5949ae989102ec
Mashi0629 <poboshiniomashi@gmail.com>
2026-03-24 18:21:43 +0530

    Update LOGIN (generate token)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const express = require("express");
 const router = express.Router();
@@ -38,6 +39,34 @@ router.post("/login", async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    //  CREATE TOKEN
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
     // compare password
     const isMatch = await bcrypt.compare(password, user.password);
