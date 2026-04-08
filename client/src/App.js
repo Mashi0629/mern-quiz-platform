0c6bc8063486e4ece10f4f7a6ec80371c9d0b683
Mashi0629 <poboshiniomashi@gmail.com>
2026-04-08 18:59:48 +0530

    Show Login in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <h1>Quiz App 🚀</h1>
+
       <Register />
+      <hr />
+      <Login />
     </div>
   );
 }
