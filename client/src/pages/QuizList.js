d6e09d05452de0bccafb4713ae32fa8ac9e06410
Mashi0629 <poboshiniomashi@gmail.com>
2026-04-09 21:55:15 +0530

    Connect from Quiz List

--- client/src/pages/QuizList.js
+++ client/src/pages/QuizList.js
@@ -1,30 +1,35 @@
 import React, { useEffect, useState } from "react";
 import API from "../api/api";
+import QuizPage from "./QuizPage";
 
 function QuizList() {
   const [quizzes, setQuizzes] = useState([]);
+  const [selectedQuizId, setSelectedQuizId] = useState(null);
 
   useEffect(() => {
     const fetchQuizzes = async () => {
-      try {
-        const res = await API.get("/quizzes");
-        setQuizzes(res.data);
-      } catch (err) {
-        console.log(err);
-      }
+      const res = await API.get("/quizzes");
+      setQuizzes(res.data);
     };
 
     fetchQuizzes();
   }, []);
+
+  //  If quiz selected, show quiz page
+  if (selectedQuizId) {
+    return <QuizPage quizId={selectedQuizId} />;
+  }
 
   return (
     <div>
       <h2>Available Quizzes 📋</h2>
 
       {quizzes.map((quiz) => (
-        <div key={quiz._id} style={{ border: "1px solid black", margin: "10px", padding: "10px" }}>
+        <div key={quiz._id}>
           <h3>{quiz.title}</h3>
-          <p>{quiz.questions.length} Questions</p>
+          <button onClick={() => setSelectedQuizId(quiz._id)}>
+            Attempt Quiz
+          </button>
         </div>
       ))}
     </div>
