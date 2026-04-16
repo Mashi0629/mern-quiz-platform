3eb40562ffa241914d7cbe9ee603c415a7f97d02
Mashi0629 <poboshiniomashi@gmail.com>
2026-04-16 12:09:10 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import QuizList from "./pages/QuizList";
 import Leaderboard from "./pages/Leaderboard";
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-600 via-blue-500 to-indigo-600 flex flex-col items-center p-6">
-      
-      <h1 className="text-4xl font-bold text-white mb-6">
-        Quiz App 
-      </h1>
-
-      <div className="w-full max-w-xl space-y-6">
-        <Register />
-        <Login />
-        <QuizList />
-        <Leaderboard />
-      </div>
-
-    </div>
-  );
-}
+import Navbar from "./components/Navbar";
 
 function App() {
   const [page, setPage] = useState("home");
@@ -39,11 +21,9 @@ function App() {
         {page === "register" && <Register />}
 
       </div>
+
     </div>
   );
 }
-
-
-
 
 export default App;
